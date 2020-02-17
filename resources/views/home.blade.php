d932a5e53146303118651c6e91184d8bc7fa1c03
estherukandu.bincom <estherukandu.bincom@gmail.com>
2020-02-17 09:33:21 +0100

    headers so far

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <img src="" alt="">
+        </div>
+
+        <div class="col-9 pt-5">
+            <div><h1> freecodecamp </h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong> 153 </strong> posts </div>
+                <div class="pr-5"><strong> 23k </strong> followers </div>
+                <div class="pr-5"><strong> 212 </strong> following </div>
+            </div>
+
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div> We're a global community of millions of people learning to code together. We're an open sourse, donor-support, 501(c)(3) nonprofile</div>
+            <div><a href="#"> www.freecodecamp.org </a></div>
+        </div>
+    </div>
+</div>
+@endsection
