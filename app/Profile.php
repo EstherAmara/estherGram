54f719526e05d84eec26a17ab14d5fffb1ffb8d4
estherukandu.bincom <estherukandu.bincom@gmail.com>
2020-03-04 16:30:11 +0100

    still on update profile picture

--- app/Profile.php
+++ app/Profile.php
@@ -7,8 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     //
+    const DEFAULT_IMAGE = 'profile/YsiLLieQBew4GWF3YUUrDGMRheCCbtUjii2wF6Lu.png';
 
     protected $guarded = [];
+
+    public function profileImage() {
+        $path = ($this->image) ? $this->image : self::DEFAULT_IMAGE;
+
+        return '/storage/'.$path;
+    }
+
     public function user() {
         return $this->belongsTo(User::class);
     }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -5,12 +5,16 @@
     <div class="row">
         <div class="col-1"></div>
         <div class="col-3 p-5">
-            <img src="/storage/{{$user->profile->image}}" class="rounded-circle w-100">
+            <img src="{{$user->profile->profileImage()}}" class="rounded-circle w-100">
         </div>
 
         <div class="col-8 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1> {{$user->username}} </h1>
+                @cannot('update', $user->profile)
+                    <button class="btn btn-primary"> Follow </button>
+                @endcannot
+
                 @can('update', $user->profile)
                     <a href="/post/create"> Add New Post</a>
                 @endcan
